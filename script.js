5508d87ab15a40360eadc1abd3208478af5ce53b
Bradley Guidry <bradleyguidry@gmail.com>
2020-08-19 11:22:14 -0500

    adds color and saves local storage

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 $(document).ready(function () {
-
   function showDate() {
-      $("#currentDay").html(moment().format("LLLL"));
-  } setInterval(showDate, 1000);
-  showDate();
-  });
+    $("#currentDay").html(moment().format("LLLL"));
+  }
 
-  
-  $(".saveBtn").click(function () {
-    var userInput = $(this).siblings(".user-input").val()
-    var hourBlock = $(this).siblings(".user-input")
-    localStorage.setItem(userInput,hourBlock);
-    console.log(userInput)
+  function calTimeClass(hour) {
+    if (hour === moment().hours()) {
+      return "present";
+    } else if (hour > moment().hours());{
+      return "future";
+    } 
+  }
+
+  setInterval(showDate, 1000);
+  showDate();
+  $(".row").each(function (index) {
+    var hourIndex = index + 9;
+    var savedInput = localStorage.getItem("Block" + hourIndex);
+    var timeClass = calTimeClass(hourIndex);
+    $(this).children("input").val(savedInput);
+    $(this).children("input").addClass(timeClass);
   });
-  
+  $(".saveBtn").click(function () {
+    console.log(this);
+    var userInput = $(this).siblings(".user-input").val();
+    var hourBlock = $(this).siblings(".user-input").attr("id");
+    localStorage.setItem(hourBlock, userInput);
+    console.log(hourBlock, userInput);
+  });
+});
+
+//disable input field in the past ...use bootstrap to find disable (input)
